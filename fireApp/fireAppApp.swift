4af1d43540c492694dffa2fbcd46e228f65b3b19
David Svensson <davidsvson@gmail.com>
2021-02-02 09:19:17 +0100

    Initial Commit

---
+++ fireApp/fireAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  fireAppApp.swift
+//  fireApp
+//
+//  Created by David Svensson on 2021-02-02.
+//
+
+import SwiftUI
+
+@main
+struct fireAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
